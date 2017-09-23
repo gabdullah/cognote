da6cb1d0af0b8148fd87d38f61d9c247eb5c4938
Ben Holland <bhollan5@kent.edu>
2017-09-23 14:47:41 -0400

    Initial vue environment

---
+++ cognote/src/components/Home.vue
@@ -0,0 +1,75 @@
+<template>
+<div id="container">
+  <div id="header">
+    <h1>COGNOTE</h1>
+    <h3 id="subtitle">AN EASIER WAY TO STUDY</h3>
+  </div>
+
+<p>Type your notes below, and hit "Submit" to make a quiz!!</p>
+  <form action="" method="post">
+<input id="name" type="text" placeholder="Enter your name"><br><br>
+  
+  <textarea id="notes" type="text">
+    
+  </textarea><br>
+    <button>Submit</button>
+  </form>
+    
+    
+    
+    </div>
+</template>
+
+<style scoped>
+    
+#container {
+  font-family: sans-serif;
+  position: absolute;
+  left: 0px;
+  top: 0px;
+  height: 100%;
+  width: 100%;
+  text-align: center;
+  justify-content: center;
+  flex-flow: column;
+  background: #46CDCF;
+}
+#name {
+  background-color: #F5FEFF;
+  width:80vh;
+  height:25px;
+  outline: none;
+  border: none;
+}
+#notes {
+  height: 20vh;
+  width: 80%;
+}
+
+
+#header {
+  width: 100%;
+  margin-top: 0px;
+  padding: 0px;
+  height: 140px;
+  background: #48466D;
+  color: white;
+  position: relative;
+}
+h1 {
+  position: absolute;
+  bottom: 20px;
+  left: 3px;
+  font-size: 50px;
+  font-style: italic;
+  margin: 0px;
+}
+
+#subtitle {
+  position: absolute;
+  bottom: -5px;
+  font-style: italic;
+  left: 10px;
+  font-size: 12px;
+}
+</style>
