82e2b03bb7b5718a13edb4dd07e3d9cba46a5914
Ben Holland <bhollan5@kent.edu>
2017-09-24 12:25:20 -0400

    dumb aesthetic thing

--- cognote/src/components/CognoteHeader.vue
+++ cognote/src/components/CognoteHeader.vue
@@ -29,7 +29,12 @@ h1 {
   font-style: italic;
   margin: 0px;
   padding-bottom: 30px;
+    transition-duration: 3s;
 }
+    
+    h1:hover {
+        letter-spacing: 20px;
+    }
 
 #subtitle {
   position: absolute;
